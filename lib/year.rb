a0a807f17cb28643b823fcc005df6026d7d2ecd1
Ben Blackburn <benjamin.blackburn@gmail.com>
2014-01-08 15:47:18 -0600

    final

--- lib/year.rb
+++ lib/year.rb
@@ -31,7 +31,7 @@ class Year
   	space = 20 - jan[6].length 
   	jan[6] << (" " * space)
   	space = 20 - jan[7].length unless jan[7].nil?
-  	jan[7] << (" " * space) unless jan[7].nil?
+  	jan[7] << (" " * space) unless jan[7].nil? 
 
   	feb = year_array[1]
   	space = 20 - feb[6].length 
@@ -89,42 +89,30 @@ class Year
 
     # Print January-March
   	"#{'January'.center(20)}  #{'February'.center(20)}  #{' ' * 7 + 'March'}" + "\n" +
-  	"#{jan[1]}  #{feb[1]}  #{mar[1]}" + "\n" +
-  	"#{jan[2]}  #{feb[2]}  #{mar[2]}" + "\n" +
-  	"#{jan[3]}  #{feb[3]}  #{mar[3]}" + "\n" +
-  	"#{jan[4]}  #{feb[4]}  #{mar[4]}" + "\n" +
-  	"#{jan[5]}  #{feb[5]}  #{mar[5]}" + "\n" +
-  	"#{jan[6]}  #{feb[6]}  #{mar[6]}" + "\n" +
-    "#{jan[7] || '                    '}  #{feb[7] || '                    '}  #{mar[7]}" + "\n" +
+  	"#{jan[1]}  #{feb[1]}  #{mar[1]}" + "\n" + "#{jan[2]}  #{feb[2]}  #{mar[2]}" + "\n" +
+  	"#{jan[3]}  #{feb[3]}  #{mar[3]}" + "\n" + "#{jan[4]}  #{feb[4]}  #{mar[4]}" + "\n" +
+  	"#{jan[5]}  #{feb[5]}  #{mar[5]}" + "\n" + "#{jan[6]}  #{feb[6]}  #{mar[6]}" + "\n" +
+    "#{jan[7] or '                    '}  #{feb[7] or '                    '}  #{mar[7]}" + "\n" +
 
     # Print April-June
   	"#{'April'.center(20)}  #{'May'.center(20)}  #{' ' * 8 + 'June'}" + "\n" +
-  	"#{apr[1]}  #{may[1]}  #{jun[1]}" + "\n" +
-  	"#{apr[2]}  #{may[2]}  #{jun[2]}" + "\n" +
-  	"#{apr[3]}  #{may[3]}  #{jun[3]}" + "\n" +
-  	"#{apr[4]}  #{may[4]}  #{jun[4]}" + "\n" +
-  	"#{apr[5]}  #{may[5]}  #{jun[5]}" + "\n" +
-  	"#{apr[6]}  #{may[6]}  #{jun[6]}" + "\n" +
-    "#{apr[7] || '                    '}  #{may[7] || '                    '}  #{jun[7]}" + "\n" +
+  	"#{apr[1]}  #{may[1]}  #{jun[1]}" + "\n" + "#{apr[2]}  #{may[2]}  #{jun[2]}" + "\n" +
+  	"#{apr[3]}  #{may[3]}  #{jun[3]}" + "\n" + "#{apr[4]}  #{may[4]}  #{jun[4]}" + "\n" +
+  	"#{apr[5]}  #{may[5]}  #{jun[5]}" + "\n" + "#{apr[6]}  #{may[6]}  #{jun[6]}" + "\n" +
+    "#{apr[7] or '                    '}  #{may[7] or '                    '}  #{jun[7]}" + "\n" +
 
     # Print July-September
   	"#{'July'.center(20)}  #{'August'.center(20)}  #{' ' * 5 + 'September'}" + "\n" +
-  	"#{jul[1]}  #{aug[1]}  #{sep[1]}" + "\n" +
-  	"#{jul[2]}  #{aug[2]}  #{sep[2]}" + "\n" +
-  	"#{jul[3]}  #{aug[3]}  #{sep[3]}" + "\n" +
-  	"#{jul[4]}  #{aug[4]}  #{sep[4]}" + "\n" +
-  	"#{jul[5]}  #{aug[5]}  #{sep[5]}" + "\n" +
-  	"#{jul[6]}  #{aug[6]}  #{sep[6]}" + "\n" +
-  	"#{jul[7] || '                    '}  #{aug[7] || '                    '}  #{sep[7]}" + "\n" +
+  	"#{jul[1]}  #{aug[1]}  #{sep[1]}" + "\n" + "#{jul[2]}  #{aug[2]}  #{sep[2]}" + "\n" +
+  	"#{jul[3]}  #{aug[3]}  #{sep[3]}" + "\n" + "#{jul[4]}  #{aug[4]}  #{sep[4]}" + "\n" +
+  	"#{jul[5]}  #{aug[5]}  #{sep[5]}" + "\n" + "#{jul[6]}  #{aug[6]}  #{sep[6]}" + "\n" +
+  	"#{jul[7] or '                    '}  #{aug[7] or '                    '}  #{sep[7]}" + "\n" +
 
     #Print October-December  
   	"#{'October'.center(20)}  #{'November'.center(20)}  #{' ' * 6 + 'December'}" + "\n" +
-  	"#{oct[1]}  #{oct[1]}  #{oct[1]}" + "\n" + 
-  	"#{oct[2]}  #{nov[2]}  #{dec[2]}" + "\n" +
-  	"#{oct[3]}  #{nov[3]}  #{dec[3]}" + "\n" +
-  	"#{oct[4]}  #{nov[4]}  #{dec[4]}" + "\n" +
-  	"#{oct[5]}  #{nov[5]}  #{dec[5]}" + "\n" +
-  	"#{oct[6]}  #{nov[6]}  #{dec[6]}" + "\n" +
-  	"#{oct[7] || '                    '}  #{nov[7] || '                    '}  #{dec[7]}" + "\n"
+  	"#{oct[1]}  #{oct[1]}  #{oct[1]}" + "\n" + "#{oct[2]}  #{nov[2]}  #{dec[2]}" + "\n" +
+  	"#{oct[3]}  #{nov[3]}  #{dec[3]}" + "\n" + "#{oct[4]}  #{nov[4]}  #{dec[4]}" + "\n" +
+  	"#{oct[5]}  #{nov[5]}  #{dec[5]}" + "\n" + "#{oct[6]}  #{nov[6]}  #{dec[6]}" + "\n" +
+  	"#{oct[7] or '                    '}  #{nov[7] or '                    '}  #{dec[7]}" + "\n"
 	end 
 end
