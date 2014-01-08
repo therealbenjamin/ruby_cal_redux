61e9a2d7894c034092c400c877929474bc250dbb
Ben Blackburn <benjamin.blackburn@gmail.com>
2014-01-08 12:24:41 -0600

    year prints, 7th row still has whitespace issue

--- cal.rb
+++ cal.rb
@@ -5,13 +5,14 @@ if ARGV.size == 1
 	month = 1
 	year = ARGV[0].to_i
 	cal = Year.new(month, year)
-	cal.print_year
+	puts cal.print_year 
 else 
 	month = ARGV[0].to_i
 	year = ARGV[1].to_i	
 	cal = Month.new(month, year)
 	puts cal.month_format
 end 
+
 
 
 
